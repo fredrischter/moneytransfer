44f964ffbbaef3060bdaf353894f87a1ee5f255a
fredrischter <fredrischter@gmail.com>
2018-11-22 14:19:31 -0200

    Some kind of possible exceptions.

---
+++ src/main/java/com/bank/moneytransfer/exception/MoneyTransferException.java
@@ -0,0 +1,11 @@
+package com.bank.moneytransfer.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.INTERNAL_SERVER_ERROR)
+public class MoneyTransferException extends Exception {
+	
+	private static final long serialVersionUID = 2319933158956427962L;
+
+}
