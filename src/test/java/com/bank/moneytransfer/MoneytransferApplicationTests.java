ef96b081e9bbdd27b8630d45d7f60057e191d644
fredrischter <fredrischter@gmail.com>
2018-11-22 13:37:09 -0200

    Starting the project.

---
+++ src/test/java/com/bank/moneytransfer/MoneytransferApplicationTests.java
@@ -0,0 +1,16 @@
+package com.bank.moneytransfer;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MoneytransferApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
