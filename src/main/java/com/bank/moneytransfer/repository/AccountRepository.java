bff94ef463a1c807f8c0159397f6cd1c45b9aaf8
fredrischter <fredrischter@gmail.com>
2018-11-26 17:41:34 -0200

    Annotating repository as repository.

--- src/main/java/com/bank/moneytransfer/repository/AccountRepository.java
+++ src/main/java/com/bank/moneytransfer/repository/AccountRepository.java
@@ -1,9 +1,11 @@
 package com.bank.moneytransfer.repository;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import com.bank.moneytransfer.model.Account;
 
+@Repository
 public interface AccountRepository extends CrudRepository<Account, String> {
 	
 }
